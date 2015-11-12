dbb1cf34e553be0e02a7a6782c2d4deffc07be8b
VytasHub <vytasvaiciulisgti@gmail.com>
2015-11-12 14:28:32 +0000

    Chost Prefab

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * 2f * Time.deltaTime);
+        anim.SetTrigger("Death");
+    }
+}
